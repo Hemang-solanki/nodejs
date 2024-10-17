30193a78dd053b4f68da83ff9c953150f681ee1d
Hemang-solanki <157679625+Hemang-solanki@users.noreply.github.com>
2024-10-17 13:00:23 +0530

    local moduals

--- Day1/app.js
+++ Day1/app.js
@@ -6,6 +6,6 @@
  const {add,sub,mult} = require("./math.js")
 
 
-   console.log(add(4,5))
-   console.log(sub(4,5))
-   console.log(mult(4,5))
+   console.log(add(3,5))
+   console.log(sub(5,5))
+   console.log(mult(10,5))
